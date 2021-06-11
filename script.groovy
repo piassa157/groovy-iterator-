5665bb39aebdbafb15c4737fbc9ef8e088cfd4ac
piassa157 <lindaum98@gmail.com>
2021-06-11 11:10:53 -0300

    script groovy

---
+++ script.groovy
@@ -0,0 +1,32 @@
+xptoFunction =  {def fooBar -> 
+
+def getAllX = []
+def getAllY = []
+
+
+    if(!(X == 0 && Y == 0)){
+        return false
+    }
+
+    fooBar.each{ items ->
+
+        if(items.value == 'X'){
+            getAllX.add(items)
+        }
+
+        if(items.value == 'Y'){
+            getAllY.add(items)
+        }
+
+    }
+
+
+    getAllX.each{ X ->
+        getAllY.each{Y ->
+            if(X.xpto == Y.xpto){
+                /* you action here */
+            }
+        } 
+    }
+
+}
